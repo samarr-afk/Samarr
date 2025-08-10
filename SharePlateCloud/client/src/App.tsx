470bd6e0a679968d7c1153ff9ec92208b729d573
samarr-afk <samarrkotwal@gmail.com>
2025-08-10 14:43:08 +0530

    Update App.tsx

--- SharePlateCloud/client/src/App.tsx
+++ SharePlateCloud/client/src/App.tsx
@@ -1,32 +1,16 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/home";
-import Admin from "@/pages/admin";
+import { Route, Switch } from "wouter";
+import Home from "./pages/home";
+import Admin from "./pages/admin";
+import NotFound from "./pages/not-found";
 
-function Router() {
+export default function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/admin" component={Admin} />
-      <Route path="/d/:id" component={Home} />
-      <Route component={NotFound} />
+      <Route>
+        <NotFound />
+      </Route>
     </Switch>
   );
 }
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
-
-export default App;

1c56e9c33c9cca39442ff8df9c990d6dbe1b5827
samarr-afk <samarrkotwal@gmail.com>
2025-08-10 15:13:41 +0530

    Update main.tsx

--- SharePlateCloud/client/src/main.tsx
+++ SharePlateCloud/client/src/main.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
